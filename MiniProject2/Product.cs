b4342a1e514b72b876b3aa819705895dc6a8ba47
mattiasvlex <mattias.vinje@gmail.com>
2024-03-27 18:33:25 +0100

    Added Product.cs

---
+++ MiniProject2/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProject2
+{
+    internal class Product
+    {
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public Product(string category, string name, int price) {
+            Category = category;
+            Name = name;
+            Price = price;
+        }
+    }
+}
